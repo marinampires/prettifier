643245f2f033038ece389a808e53fc8d4684dcc1
Marina Melo <marinampires@gmail.com>
2016-01-02 13:43:56 -0200

    included test for million number

--- prettifier/number_prettifier.py
+++ prettifier/number_prettifier.py
@@ -5,6 +5,20 @@ def prettifier(number):
             return number
         number_prettified = ""
         
+        if number_is_million(number):
+          return  convert_to_million(number)
         return number_prettified
     else:
         raise ValueError('The type is not a numeric value') 
+
+def number_is_million(number):        
+    return number/million > 0
+
+def convert_to_million(number):
+    integer = number/million
+    mod = number % million
+    if mod == 0:
+        return "%dM" % integer
+    else:
+        return "%d.%dM" % (integer, mod)
+    
--- tests/number_prettifier_test.py
+++ tests/number_prettifier_test.py
@@ -4,24 +4,27 @@ from prettifier.number_prettifier import prettifier
 
 class TestNumberPrettifier(unittest.TestCase):
 
-  def test_validate_number_with_3_digits(self):
+  def test_number_with_3_digits(self):
     number = 532
     number_prettified = prettifier(number)
     self.assertEqual(number, number_prettified)
 
-  def test_validate_number_with_5_digits(self):
+  def test_number_with_5_digits(self):
     number = 14532
     number_prettified = prettifier(number)
     self.assertEqual(number, number_prettified)
 
-  def test_validate_not_numerics_input(self):
+  def test_not_numerics_input(self):
     number = "number"
     with self.assertRaises(ValueError):
         prettifier(number)
   
-  #def validate_million_number(self):
+  def test_million_number(self):
+    number = 1000000
+    number_prettified = prettifier(number)
+    self.assertEqual("1M", number_prettified)
 
-  #def validate_decimal_number_not_integer(self):
+  #def test_decimal_million_number(self):
 
 if __name__ == '__main__':
     unittest.main()
